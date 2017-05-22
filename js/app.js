107240dea636ceea4f2f0afad2b63b4fb1c909a4
visergra <visergra@hotmail.com>
2017-05-22 22:54:28 +0200

    countdown done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 window.onload = function() {
-  gameNumbers = new GameMemorizeNumbers();
-  gameNumbers._generateSequence();
+  var playButton = document.getElementById('play');
+  playButton.onclick = playGame;
   //document.addEventListener("keydown", moveListeners);
-
 };
 
 function showSequence() {
-
+  gameNumbers.board.forEach(function(row, rowIndex) {
+    row.forEach(function(cell, cellIndex) {
+      if (cell !== null) {
+        var numberContainer = document.getElementById(rowIndex + "-" + cellIndex);
+        numberContainer.classList.add("active");
+        numberContainer.innerHTML = cell;
+      }
+    });
+  });
 }
 
 function checkUserInput() {
@@ -24,10 +31,36 @@ function brainAge() {
 function playGame() {
   gameNumbers = new GameMemorizeNumbers();
   gameNumbers._generateSequence();
+  gameNumbers._shuffleBoard();
+  cleanNotebook();
+  countDown();
 }
 
 function cleanNotebook() {
+  gameNumbers.board.forEach(function(row, rowIndex) {
+    row.forEach(function(cell, cellIndex) {
+      var numberContainer = document.getElementById(rowIndex + "-" + cellIndex);
+      numberContainer.classList.remove("active");
+      numberContainer.innerHTML = "";
+    });
+  });
+}
 
+function countDown() {
+  var countDownDiv = document.querySelector('.countdown');
+  var seconds = countDownDiv.innerHTML;
+  countDownDiv.classList.add("active");
+  if (seconds == 1) {
+    countDownDiv.classList.remove("active");
+    //temp = document.getElementById('countdown');
+    countDownDiv.innerHTML = 4;
+    showSequence();
+    return;
+  }
+  seconds--;
+  //temp = document.getElementsByClassName('countdown');
+  countDownDiv.innerHTML = seconds;
+  timeoutCountDown = setTimeout(countDown, 1000);
 }
 
 function gameOver() {
